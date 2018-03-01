2eaab46ca11cab19bb93e75b949eb4f941873364
Dpens <514682604@qq.com>
2018-03-01 12:46:38 +0800

    <mod>counting_sort.c cancle return and sort array which user inputs

--- counting_sort/counting_sort.c
+++ counting_sort/counting_sort.c
@@ -3,36 +3,37 @@
 #include<time.h>
 #define n 10
 #define m 100
-int * counting_sort(int A[n],int B[n]); //A is thee array which needs to be sorted,B is used to store sorted array
+void counting_sort(int A[n]); //A is thee array which needs to be sorted
 
 int main(void)
 {
 	int A[n] = { 0 };
-	int B[n] = { 0 };
 	srand((unsigned int)time(0));
 	for (int i = 0; i < n; i++)
 	{
 		A[i] = rand() % m;
 		printf("A[%d] = %d\n", i, A[i]);
 	}
-	int *p = counting_sort(A, B);
+	counting_sort(A);
 	for (int i = 0; i < n; i++)
-		printf("B[%d] = %d\n", i, *(p + i));
+		printf("A[%d] = %d\n", i, *(A + i));
 	getchar();
 	return 0;
 }
 
-int *counting_sort(int A[n],int B[n])
+void counting_sort(int A[n])
 {
 	int C[m] = { 0 };
+	int B[n] = { 0 };
 	for (int i = 0; i < n; i++)
-		C[A[i]] += 1;
+		B[i] = A[i];
+	for (int i = 0; i < n; i++)
+		C[B[i]] += 1;
 	for (int i = 1; i < m; i++)
 		C[i] += C[i - 1];
 	for (int i = n - 1; i >= 0; i--)
 	{
-		B[C[A[i]] - 1] = A[i];    //C[A[i]] - 1 because A[i] must minus itself
-		C[A[i]] -= 1;
+		A[C[B[i]] - 1] = B[i];    //C[A[i]] - 1 because A[i] must minus itself
+		C[B[i]] -= 1;
 	}
-	return B;
 }
