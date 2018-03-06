24569b0ed433b96b31916be73fff6945a63b9861
Dpens <514682604@qq.com>
2018-03-06 10:26:55 +0800

    <mod> bucket_sort.cpp

--- bucket_sort/bucket_sort.cpp
+++ bucket_sort/bucket_sort.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 #include<cmath>
 
-namespace bucket {
+namespace bucket {                          
 	typedef struct _bucket
 	{
 		double a;
@@ -13,14 +13,14 @@ namespace bucket {
 void bucket_sort(double *A, int n)
 {
 	using namespace bucket;
-	Bucket B[10];
+	Bucket B[10];                     
 	std::vector<int>C(n);
 	for (int i = 0; i < 10; i++)
 	{
 		B[i].next = NULL;
 		B[i].a = 0;
 	}
-	double biggest = A[0];
+	double biggest = A[0];            //找到数组中最大的元素,并计算他的位数
 	C[0] = A[0];
 	for (int i = 1; i < n; i++)
 	{
@@ -36,9 +36,9 @@ void bucket_sort(double *A, int n)
 	}
 	for (int i = 0; i < n; i++)
 	{
-		C[i] /= pow(10, len - 1);
+		C[i] /= pow(10, len - 1);           //将所有数组元素都除以10的最大数的位数-1次方,并以此为依据决定他们将被放置的桶
 	}
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < n; i++)            //将数组中的数据放到桶中
 	{
 		if (B[C[i]].next == NULL)
 		{
@@ -55,7 +55,7 @@ void bucket_sort(double *A, int n)
 			{
 				current = current->next;
 			}
-			if (current->next == NULL)
+			if (current->next == NULL)          //将该结构放在链表结尾
 			{
 				current->next = temp;
 				temp->a = A[i];
@@ -63,8 +63,8 @@ void bucket_sort(double *A, int n)
 			}
 			else
 			{
-				temp->next = current->next;
-				current->next = temp;
+				temp->next = current->next;   //将该结构放在他正确位置的下一个 如3放在1,2,4,5中的4,5之间
+				current->next = temp;			//然后将3和4交换
 				double Temp = current->a;
 				current->a = A[i];
 				temp->a = Temp;
@@ -72,7 +72,7 @@ void bucket_sort(double *A, int n)
 		}
 	}
 	int j = 0;
-	for (int i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)      //将桶中数据拿出放入数组中
 	{
 		Bucket *current = B[i].next;
 		while (current != NULL)
