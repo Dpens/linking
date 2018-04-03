0d131d429855560f8591a3fa52c5b8e9f3abcc76
Dpens <514682604@qq.com>
2018-04-03 22:54:33 +0800

    <mod>

--- awesome_python3_webapp/www/test_login_signin.py
+++ awesome_python3_webapp/www/test_login_signin.py
@@ -1,7 +1,11 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,flash
 import mysql.connector
+import tkinter
+import tkinter.messagebox
+from tkinter import *
 
 app = Flask(__name__)
+app.secret_key = 'some_secret'
 
 conn = mysql.connector.connect(user='root',password='Password',database='userchar')
 cursor = conn.cursor()
@@ -12,9 +16,23 @@ cursor.execute('create table if not exists \
 #cursor.close()
 
 
+#@app.route('/')
+#def hello(name='Dpens'):
+    #return render_template('hello.html', name=name)
+
+def turn():
+	if request.methods == 'POST':
+		if request.form.get("sign in") == 'sign_in':
+			return render_template('hello.html')
+		elif request.form.get("log in") == 'log_in':
+			return render_template('log_in.html')
+
+
 @app.route('/')
-def hello(name='Dpens'):
-    return render_template('hello.html', name=name)
+def start():
+	return render_template('start.html')
+	turn()
+	
 
 @app.route('/',methods=['GET','POST'])
 def index():
@@ -22,10 +40,23 @@ def index():
 		xusername = request.form.get("username")
 		xpassword = request.form.get("psw")
 		xid = '0'
-		cursor.execute('insert into userdb (id,username,password) values("%s","%s","%s")'%(xid,xusername,xpassword))
-		conn.commit()
-		cursor.close()
-		return render_template('sign_in.html')
+		if request.form.get("sign") == '注册':
+			cursor.execute('select * from userdb where id > 0')
+			results = cursor.fetchall()
+			for row in results:
+				if xusername == row[1]:
+					#tkinter.messagebox.showerror('error','%s already exists'%xusername)
+					flash('%s already exists'%xusername)
+					xid = '1'
+		else:
+			return render_template('hello.html')
+		if xid == '0':
+			cursor.execute('insert into userdb (id,username,password) values("%s","%s","%s")'%(xid,xusername,xpassword))
+			conn.commit()
+			cursor.close()
+			return render_template('sign_in.html')
+		return render_template('hello.html')
+
 
 if __name__ == '__main__':
 	app.run(debug = True)
